3d08bdd0cb45a71e8712c7e1107bde5c32065429
rock-007 <umair.ashraf2019@gmail.com>
2021-04-28 23:30:12 +0100

    fixed a bug in Appointment route

--- controllers/appointments.py
+++ controllers/appointments.py
@@ -13,32 +13,19 @@ import pdb
 appointment_blueprint = Blueprint("appointments", __name__)
 
 
-
-
-# @appointment_blueprint.route("/appointments", methods=["GET"])
-# def appointment_form():
-#     result = []
-#     return render_template('/appointments/index.html', result= result)
-
-
 @appointment_blueprint.route("/appointments", methods=["GET"])
 def book_appointment_form():
-    #verify_customer\pet_details
-    # result = []
-    # customer_id = customer_repository.search_id(request.form['customer_first_name'],request.form['customer_last_name'])
-    # pet = pet_repository.search(request.form['pet_name'], customer_id)
+
 
     vets = vet_repository.select_vets()
     day_time_form = Availability()
     day_time_form.set_day_time()
     day_time_form_keys = day_time_form.day_time.keys()
     vets_availabilty = vet_repository.available_appointments(vets)
-    # result.append(pet)
-    # result.append(vets)
-    #result.append(vets_availabilty)
+
     
     all_appointments = appointment_repository.all_appointments()
-    #pdb.set_trace()
+    
     return render_template('/appointments/index.html', vets_availabilty=vets_availabilty, day_time_form_keys= day_time_form_keys, all_appointments= all_appointments )
 
 @appointment_blueprint.route("/appointments", methods=["POST"])
@@ -47,8 +34,9 @@ def book_appointment():
     slot_selected = request.form['slot_selected']
     pet_name = request.form['pet_name']
     pet_date_of_birth = request.form['pet_date_of_birth']
+    vet_id= request.form['vet_id']
     
     #book appointment
-    result1 = appointment_repository.book_appoitment(date_selected, slot_selected, pet_name, pet_date_of_birth)
-    
-    return render_template ('/index.html')
+    result1 = appointment_repository.book_appoitment(date_selected, slot_selected, pet_name, pet_date_of_birth, vet_id)
+    pdb.set_trace()
+    return redirect(url_for('appointments.book_appointment_form'))
--- repositories/appointment_repository.py
+++ repositories/appointment_repository.py
@@ -2,12 +2,12 @@ from db.run_sql import run_sql
 import repositories.pet_repository as pet_repository
 
 import pdb
-def book_appoitment(date_selected, slot_selected, pet_name, pet_date_of_birth):
+def book_appoitment(date_selected, slot_selected, pet_name, pet_date_of_birth, vet_id):
     #find pet id with pet name and date of birth
     pet = pet_repository.search_pet_by_name_date_of_birth(pet_name, pet_date_of_birth)
 
     sql ="INSERT INTO appointments(date, time, vet_id, pets_id) values(%s, %s, %s, %s) RETURNING id"
-    values= [date_selected, slot_selected, 2, pet['id']]
+    values= [date_selected, slot_selected, vet_id, pet['id']]
     result = run_sql(sql,values)
     return result
 
